2c016903692686670debfad26fb8430a2fb29330
King Wang <badly1984@hotmail.com>
2023-12-30 21:23:20 +0800

    Update ccH5js.js

--- ccH5js.js
+++ ccH5js.js
@@ -39,7 +39,7 @@ var ccH5 = {
       progressbar_enable: 1, // 1可以拖动  0不可拖动
       showRateBtn: rateState,
       rate_allow_change: rateState, //是否显示倍速  ture 显示 false 不现实
-      rate_array: [0.5, 1, 1.5, 2, 16],
+      rate_array: [0.5, 1, 1.5, 2, 4, 8, 16],
       rate: 1,
       isShare: "false", //是否显示分享
       playtype: checkVideo(), // 1 h5 默认 CC
